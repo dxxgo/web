2fb097128fe93c9cf5d03a2dbc463a4d84e593fb
Diêgo Ferreira <85963098+dxxgo@users.noreply.github.com>
2022-04-26 18:09:24 -0300

    att

--- image_gallery/js/script.js
+++ image_gallery/js/script.js
@@ -5,18 +5,30 @@ let galleryResults = document.querySelector('.gallery__results');
 let galleryAllImages = document.querySelector('.gallery__images-container')
 let searchInput = document.querySelector('.gallery__search input')
 let allImagesBtn = document.querySelector('#allImages');
+let galleryContainer = document.querySelector('.gallery-container')
+let displayImagesQtd = document.querySelector('.qnt-imagens');
 
+searchInput.addEventListener('keypress', (tecla) => {
+    if (tecla.key === 'Enter') {
+        searchBtn.click();
+    }
+})
 
 allImagesBtn.addEventListener('click', () => {
     galleryAllImages.classList.remove('displayNone');
     galleryResults.classList.add('displayNone');
+    allImagesBtn.style.opacity = '1';
+    displayImagesQtd.classList.add('displayNone');
+    galleryContainerWidth('900px');
 })
 
 searchBtn.addEventListener('click', () => {
+    galleryContainerWidth('900px');
     let inputSearch = document.querySelector('.gallery__search input');
     searchImage(formatList(inputSearch.value.split(' ')));
     galleryAllImages.classList.add('displayNone');
     galleryResults.classList.remove('displayNone');
+    allImagesBtn.style.opacity = '.7'
 })
 
 imagesList.forEach((img, indice) => {
@@ -27,8 +39,14 @@ imagesList.forEach((img, indice) => {
     });
 })
 
+function galleryContainerWidth(width) {
+    galleryContainer.style.width = width;
+}
+
 function galleryResultsHTML(results) {
+    document.querySelector('.qnt-imagens span').textContent = results.length;
     if (results.length === 0) {
+        displayImagesQtd.classList.add('displayNone');
         galleryResults.innerHTML = (
             `<p class="noneResults">
             Nenhuma imagem<br>encontrada.
@@ -36,6 +54,15 @@ function galleryResultsHTML(results) {
         );
 
     } else {
+        displayImagesQtd.classList.remove('displayNone');
+        if (results.length === 1) {
+            galleryContainerWidth('400px');
+        } else if (results.length === 2) {
+            galleryContainerWidth('600px');
+        } else if (results.length === 3) {
+            galleryContainerWidth('800px')
+        }
+
         galleryResults.innerHTML = '';
         results.forEach((e) => {
             let src = e.img.getAttribute('src');
@@ -56,10 +83,11 @@ function searchImage(inputSearch) {
             if (img.keywords.includes(key.toUpperCase())) {
                 results.push(img);
             }
-        });
+        })
 
     })
-    galleryResultsHTML(results);
+    let uniqueResults = [...new Set(results)];
+    galleryResultsHTML(uniqueResults);
 }
 
 function formatList(list) {
@@ -75,6 +103,5 @@ function formatList(list) {
             }
         })
         // remove itens repetidos da lista
-    let uniqueList = [...new Set(newList)]
-    return uniqueList;
+    return newList;
 }
